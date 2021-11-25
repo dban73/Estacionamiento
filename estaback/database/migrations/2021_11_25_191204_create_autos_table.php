97031be8b7835df455d1aaab78a72beceb6c3ac8
Dban73 <dban95@hotmail.com>
2021-11-25 15:32:27 -0400

    db migrate

--- estaback/database/migrations/2021_11_25_191204_create_autos_table.php
+++ estaback/database/migrations/2021_11_25_191204_create_autos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateClientesTable extends Migration
+class CreateAutosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreateClientesTable extends Migration
      */
     public function up()
     {
-        Schema::create('clientes', function (Blueprint $table) {
+        Schema::create('autos', function (Blueprint $table) {
             $table->id();
-            $table->string("ci");
-            $table->string("nombre");
+            $table->string("placa");
+            $table->string("marca");
+            $table->unsignedBigInteger("cliente_id");
+            $table->foreign("cliente_id")->references("id")->on("clientes");
             $table->timestamps();
         });
     }
@@ -28,6 +30,6 @@ class CreateClientesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('clientes');
+        Schema::dropIfExists('autos');
     }
 }
